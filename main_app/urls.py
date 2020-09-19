12f43178b4b3f8d63a1174152bd56a6eeb129a89
skylerseifert <skylerseifert@gmail.com>
2020-09-19 12:01:11 -0700

    Add outline and home url

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('accounts/signup/', views.signup, name='signup'),
+    # home ''
+    # search from home -> orgs 'organizations/'
+    # click from orgs -> orgs id 'organizations/<int:org_id>/'
 ]
