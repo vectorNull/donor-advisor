f7be407da4f8dc6f1fed681a4b83eb15383a99cc
Eric Moore <eric@educate-ca.org>
2020-09-23 09:15:38 -0700

    Add Contact Page

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home'),
+    path('contact/', views.contact_page, name='contact'),
     path('organizations/', views.OrganizationResultsView.as_view(), name='org_results'),
     path('organizations/create', views.organization_create, name='org_create'),
 ]
